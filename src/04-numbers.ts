47b945f342cabdafbbe58c16b5f10577203e9819
xMarioOP <marioalejandro424@outlook.com>
2024-10-21 17:48:50 -0500

    Numbers

---
+++ src/04-numbers.ts
@@ -0,0 +1,22 @@
+(() => {
+  let productPrice = 100
+  productPrice = 12
+  console.log("productPrice", productPrice);
+
+  let customerAge: number = 28
+  customerAge = customerAge + 1
+  console.log("customerAge", customerAge);
+
+  let productInStock: number
+  console.log("productInStock", productInStock);
+  if (productInStock > 10) {
+    console.log("Is greater");
+  }
+
+  let discount = parseInt("123")
+  console.log("discount", discount);
+
+  let hex = 0xfff
+  console.log("hex", hex);
+  let bin = 0b1010
+})()
