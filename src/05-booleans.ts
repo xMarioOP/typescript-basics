27a1fe9d2fd7a4fb47b969aef0ef0af603aa66ee
xMarioOP <marioalejandro424@outlook.com>
2024-10-21 19:31:53 -0500

    Booleans

---
+++ src/05-booleans.ts
@@ -0,0 +1,9 @@
+(() => {
+  let isEnable = true
+  isEnable = false
+
+  let isNew: boolean = false //boolean not Boolean
+
+  const random = Math.random()
+  isNew = random >= 0.5 ? true : false
+})()
